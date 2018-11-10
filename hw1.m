ea76f12959749fc761ba60dfc0447bb85e5240f9
Eden Shazar <44927208+EdenShazar@users.noreply.github.com>
2018-11-10 19:20:53 +0200

    Added basic parameters and wing geometry
    Basically playing around, trying to understand what we need to do.

--- hw1.m
+++ hw1.m
@@ -1,10 +1,19 @@
 clc
-clear all
 close all
+clear variables
 
 %% Parameters
+xx = 15;    % last two NACA airfoil digits
+t = xx/100;    % max thickness as fraction of cord
+xs = 0;
+xf = 1;
 
 %% Wing
+yt = @(x) 5*t * (0.2969 * sqrt(x) - ...
+                 0.1260 * x - ...
+                 0.3516 * x.^2 + ...
+                 0.2843 * x.^3 - ...
+                 0.1015 * x.^4);
 
 %% Solver
 
