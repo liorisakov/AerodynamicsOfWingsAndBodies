3b2a31ed49f5866a16725b9c60070c0d5f15975c
Eden Shazar <44927208+EdenShazar@users.noreply.github.com>
2018-11-14 21:58:54 +0200

    Clean up and plots

--- hw1.m
+++ hw1.m
@@ -1,56 +1,66 @@
+% ---------------------------------- %
+%  AERODYNAMICS OF WINGS AND BODIES  %
+%             HOMEWORK 1             %
+%  Lior Isakov          Eden Shazar  %
+%   200928372            204378608   %
+% ---------------------------------- %
+
+%% Fresh start
 clc
 close all
 clear variables
 
 %% Parameters
-xx = 15;          % last two NACA airfoil digits
-c = 1;            % cord
-t = xx/100;       % max thickness as fraction of cord
+% user input
+airfoil.xx = 12;     % last two NACA airfoil digits
+xs_offset = 1e-3;    % forward offset of start of doublet line from origin
+xf_offset = 1e-3;    % backward offset of end of doublet line from cord end
+airfoil.N = 100;     % number of doublet line sections
+U_oo = 1;            % horizontal uniform flow speed
 
-x_s = 1e-3;        % start of doublet line
-x_f = c - 1e-3;    % end of doublet line
-N = 50;            % number of doublet line sections
+% calculated parameters
+airfoil.t = airfoil.xx/100;            % max thickness as fraction of cord
+doublet.xs = xs_offset;        % start of doublet line
+doublet.xf = 1 - xf_offset;    % end of doublet line
 
-U_oo = 1;
 
 %% Wing
-% doublet line
-theta_j = linspace(0, pi, N);
-c_star = x_f - x_s;
-t_j = c_star/2 * (1 - cos(theta_j)) + x_s;
-x_i = ((t_j(1:end-1) + t_j(2:end))/2).';
+% doublet line definition
+doublet.c_star = doublet.xf - doublet.xs;        % doublet line length
+theta_j = linspace(0, pi, airfoil.N+1);          % cosine sectioning
+doublet.tj = doublet.c_star/2 * (1 - cos(theta_j)) + doublet.xs;
+doublet.xi = ((doublet.tj(1:end-1) + ...
+               doublet.tj(2:end))/2).';          % middle points of sections
 
-% geometry
-Y = @(x) 5*t * (0.2969 * sqrt(x) - ...
-                0.1260 * x - ...
-                0.3516 * x.^2 + ...
-                0.2843 * x.^3 - ...
-                0.1015 * x.^4);
-
+% NACA 00xx geometry
+airfoil.Y = @(x) 5 * airfoil.t * (0.2969 * sqrt(x) - ...
+                                  0.1260 * x - ...
+                                  0.3516 * x.^2 + ...
+                                  0.2843 * x.^3 - ...
+                                  0.1015 * x.^4);
+            
 %% Solver
-A = 1./Y(x_i).*(atan((t_j(2:end) - x_i)./Y(x_i)) - ...
-                atan((t_j(1:end-1) - x_i)./Y(x_i)));
-% for i = 1:N-1
-%     for j = 1:N-1
-%         l = (t_j(j+1) - x_i(i))/Y(x_i(i));
-%         m = (t_j(j) - x_i(i))/Y(x_i(i));
-%         A2(i,j) = (atan(l) - atan(m))/Y(x_i(i));
-%         fprintf('%f ', l)
-%     end
-%     fprintf('\n')
-% end
-b = ones(N-1, 1);
+% solve A*mj = b
+A = 1./airfoil.Y(doublet.xi) .* ...
+    (atan((doublet.tj(2:end) - doublet.xi)./airfoil.Y(doublet.xi)) - ...
+     atan((doublet.tj(1:end-1) - doublet.xi)./airfoil.Y(doublet.xi)));
+b = ones(airfoil.N, 1);
+doublet.mj = (A\b).';
 
-m_j = A\b;
+%% Aerodynamic equations
+% flow components
+u_eq = @(x, y) U_oo * (1 + ...
+                       sum(doublet.mj.*((doublet.tj(2:end) - x) ./ ...
+                                        (((doublet.tj(2:end) - x).^2) + y^2) - ...
+                                        (doublet.tj(1:end-1) - x) ./ ...
+                                        (((doublet.tj(1:end-1) - x).^2) + y^2))));
+v_eq = @(x, y) - U_oo * ...
+               (sum(doublet.mj.*(y./((doublet.tj(2:end) - x).^2 + y^2) - ...
+                                 y./((doublet.tj(1:end-1) - x).^2 + y^2))));
 
-u = @(x, y) U_oo*(1 + ...
-                  sum(m_j.' .* ((t_j(2:end) - x)./(((t_j(2:end) - x).^2) + y^2) - ...
-                                (t_j(1:end-1) - x)./(((t_j(1:end-1) - x).^2) + y^2))));
-v = @(x, y) - U_oo*(sum(m_j.' .* (y./((t_j(2:end) - x).^2 + y^2) - ...
-                                  y./((t_j(1:end-1) - x).^2 + y^2))));
-% psi = @(x, y) U_oo*Y(x) - 1/(2*pi) * ...
-%               sum(m_j.' .* (atan((t_j(2:end) - x)./Y(x)) - ...
-%                             atan((t_j(1:end-1) - x)./Y(x))));
+% pressure coefficient
+Cp_eq = @(u, v) 1 - (u.^2 + v.^2)./U_oo^2;
+
 %% Plots
 % plot parameters
 lw = 1.2;    % line width
@@ -58,69 +68,171 @@ fs = 15;     % font size
 ms = 7;      % marker size
 load('colors.mat')
 
-% temp
-x = linspace(-1, 2, N);
-y = linspace(-0.5, 0.5, N);
-[X_grid, Y_grid] = meshgrid(x, y);
+% plotted results
+x_plot = linspace(1e-3, 1, 400);
+x_surf = linspace(-0.5, 1.5, 400);
+y_surf = linspace(-1, 1, 400);
+[X_grid, Y_grid] = meshgrid(x_surf, y_surf);
 X_grid = X_grid.';
 Y_grid = Y_grid.';
-for i = 1:length(x)
-    for j = 1:length(y)
-        U(i,j) = u(x(i), y(j));
-        V(i,j) = v(x(i), y(j));
+[u, v] = deal(zeros(size(X_grid)));
+[u_plus, v_plus] = deal(zeros(size(x_plot)));
+for i = 1:length(x_surf)
+    for j = 1:length(y_surf)
+        if x_surf(i) <= 0 || x_surf(i) >= 1 || ...
+           y_surf(j) >= airfoil.Y(x_surf(i)) || y_surf(j) <= - airfoil.Y(x_surf(i))
+            u(i,j) = u_eq(x_surf(i), y_surf(j));
+            v(i,j) = v_eq(x_surf(i), y_surf(j));
+        else
+            [u(i,j), v(i,j)] = deal(NaN);
+        end
     end
+    
 end
+Cp = Cp_eq(u, v);
+for i = 1:length(x_plot)
+    u_plus(i) = u_eq(x_plot(i), airfoil.Y(x_plot(i)));
+    v_plus(i) = v_eq(x_plot(i), airfoil.Y(x_plot(i)));
+end
+Cp_plus = Cp_eq(u_plus, v_plus);
 
+% FIGURE 1: airfoil geometry and doublet line
 figure
 hold on
-geometry_x = linspace(0, c);
-geometry_y = Y(geometry_x);
-patch([geometry_x, fliplr(geometry_x)], ...
-      [geometry_y, -fliplr(geometry_y)], ...
-      colors.blue, 'FaceAlpha', 0.2)
-plot(t_j, zeros(length(t_j), 1), '.', ...
-     'LineWidth', lw, 'MarkerSize', ms, 'Color', colors.red)
-plot(t_j, zeros(length(t_j), 1), ...
-     'LineWidth', lw, 'Color', colors.red)
-plot([geometry_x, fliplr(geometry_x)], ...
-     [geometry_y, -fliplr(geometry_y)], ...
-     'LineWidth', lw, 'Color', colors.blue)
+plots = plot_NACA_00xx(airfoil, doublet, true);
+title({sprintf('NACA $00%02d$ airfoil geometry ', airfoil.xx), ...
+       'and defined doublet line'}, 'FontSize', fs)
+xlabel('$\frac{x}{c}$', 'FontSize', fs)
+ylabel('$\frac{y}{c}$', 'FontSize', fs)
+legend([plots.geometry, plots.doublet], ...
+       sprintf('NACA $00%02d$ geometry', airfoil.xx), ...
+       'Doublet line sections', ...
+       'Location', 'Northeast');
 axis equal
 grid on
 hold off
 
+% FIGURES 2-3: u flow component
 figure
 hold on
-surf(X_grid, Y_grid, U, 'LineStyle', 'none')
+surf(X_grid, Y_grid, u/U_oo, 'LineStyle', 'none', 'FaceColor', 'interp')
+plot_NACA_00xx(airfoil, doublet);
+title('Flow component: $\frac{u}{U_\infty}$', 'FontSize', fs)
+xlabel('$\frac{x}{c}$', 'FontSize', fs)
+ylabel('$\frac{y}{c}$', 'FontSize', fs)
 view(2)
 colorbar
-grid on
+axis image
 hold off
 
 figure
 hold on
-surf(X_grid, Y_grid, V, 'LineStyle', 'none')
-view(3)
+plot_NACA_00xx(airfoil, doublet);
+plot(x_plot, u_plus/U_oo, 'LineWidth', lw, 'Color', colors.red)
+title('Horizontal flow component along upper surface', 'FontSize', fs)
+xlabel('$\frac{x}{c}$', 'FontSize', fs)
+ylabel('$\frac{u}{U_{\infty}}$', 'FontSize', fs)
+axis image
+axis manual
+y_limits = ylim;
+ylim([y_limits(1), 1.4]);
+grid on
+hold off
+
+% FIGURES 4-5: v flow component
+figure
+hold on
+surf(X_grid, Y_grid, v/U_oo, 'LineStyle', 'none', 'FaceColor', 'interp')
+plot_NACA_00xx(airfoil, doublet);
+title('Flow component: $\frac{v}{U_\infty}$', 'FontSize', fs)
+xlabel('$\frac{x}{c}$', 'FontSize', fs)
+ylabel('$\frac{y}{c}$', 'FontSize', fs)
+view(2)
 colorbar
-grid on
+axis image
 hold off
 
 figure
 hold on
-geometry_x = linspace(0, c);
-geometry_y = Y(geometry_x);
+plot_NACA_00xx(airfoil, doublet);
+plot(x_plot, v_plus/U_oo, 'LineWidth', lw, 'Color', colors.red)
+title('Vertical flow component along upper surface', 'FontSize', fs)
+xlabel('$\frac{x}{c}$', 'FontSize', fs)
+ylabel('$\frac{v}{U_{\infty}}$', 'FontSize', fs)
+axis image
+axis manual
+y_limits = ylim;
+ylim([y_limits(1), 0.8]);
+grid on
+hold off
+
+% FIGUREs 5-6: pressure coefficient 
+figure
+hold on
+surf(X_grid, Y_grid, Cp, 'LineStyle', 'none', 'FaceColor', 'interp')
+plot_NACA_00xx(airfoil, doublet);
+title('Pressure coefficient', 'FontSize', fs)
+xlabel('$\frac{x}{c}$', 'FontSize', fs)
+ylabel('$\frac{y}{c}$', 'FontSize', fs)
+view(2)
+colorbar
+axis image
+hold off
+
+figure
+hold on
+plot_NACA_00xx(airfoil, doublet);
+plot(x_plot, 1 - Cp_plus, 'LineWidth', lw, 'Color', colors.red)
+title('Pressure coefficient along upper surface', 'FontSize', fs)
+xlabel('$\frac{x}{c}$', 'FontSize', fs)
+ylabel('$\frac{u^2+v^2}{U_{\infty}^2}$', 'FontSize', fs)
+axis image
+axis manual
+y_limits = ylim;
+ylim([y_limits(1), 1.5]);
+grid on
+hold off
+
+% FIGURE 7: stream lines
+figure
+hold on
+plot_NACA_00xx(airfoil, doublet);
+
+starty = linspace(min(min(Y_grid)), max(max(Y_grid)), 50);
+startx = min(min(X_grid))*ones(size(starty));
+streamline(X_grid.', Y_grid.', u.', v.', startx, starty)
+
+title('Stream lines', 'FontSize', fs)
+xlabel('$\frac{x}{c}$', 'FontSize', fs)
+ylabel('$\frac{y}{c}$', 'FontSize', fs)
+xlim([min(min(X_grid)), max(max(X_grid))]);
+ylim([min(min(Y_grid)), max(max(Y_grid))]);
+axis image
+grid on
+hold off
+
+function plots = plot_NACA_00xx(airfoil, doublet, plot_doublet_line)
+% plot parameters
+lw = 1.2;    % line width
+ms = 7;      % marker size
+load('colors.mat')
+
+if nargin == 2
+    plot_doublet_line = false;
+end
+
+geometry_x = linspace(0, 1);
+geometry_y = airfoil.Y(geometry_x);
+
 patch([geometry_x, fliplr(geometry_x)], ...
       [geometry_y, -fliplr(geometry_y)], ...
       colors.blue, 'FaceAlpha', 0.2)
-plot([geometry_x, fliplr(geometry_x)], ...
-     [geometry_y, -fliplr(geometry_y)], ...
-     'LineWidth', lw, 'Color', colors.blue)
+if plot_doublet_line
+    plots.doublet = plot(doublet.tj, zeros(length(doublet.tj), 1), '- .', ...
+                         'LineWidth', lw, 'MarkerSize', ms, 'Color', colors.red);
+end
+plots.geometry = plot([geometry_x, fliplr(geometry_x)], ...
+                      [geometry_y, -fliplr(geometry_y)], ...
+                      'LineWidth', lw, 'Color', colors.blue);
+end
 
-starty = linspace(min(min(Y_grid)), max(max(Y_grid)), 50);
-startx = -1*ones(size(starty));
-streamline(X_grid.', Y_grid.', U.', V.', startx, starty)
-axis equal
-ylim([min(min(Y_grid)), max(max(Y_grid))]);
-
-grid on
-hold off
